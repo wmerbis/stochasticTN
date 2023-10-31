bb95df7b94c3bf36c4fddbe859c20868e22d6aa3
wmerbis <81095847+wmerbis@users.noreply.github.com>
2023-10-31 21:44:54 +0100

    Update __init__.py

--- stochasticTN/__init__.py
+++ stochasticTN/__init__.py
@@ -1,4 +1,4 @@
-from stochasticTN.mps import MPS, randomMPS, loadMPS
+from stochasticTN.mps import MPS, randomMPS, loadMPS, occupied_mps, uniform_mps 
 from stochasticTN.mpo import MPO, SIS_MPO, occupancy_MPO, gapMPO
 from stochasticTN.stochasticDMRG import DMRG
 from stochasticTN.operations import *
