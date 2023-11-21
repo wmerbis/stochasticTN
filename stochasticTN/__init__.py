f99797772c36cee358810aea8a7dad061df08c5f
wmerbis <wmerbis@gmail.com>
2023-11-21 16:15:59 +0100

    mps functions added and mpo canonicalize fixed

--- stochasticTN/__init__.py
+++ stochasticTN/__init__.py
@@ -1,9 +1,5 @@
 from stochasticTN.mps import *
-from stochasticTN.imps import iMPS
-from stochasticTN.impo import *
 from stochasticTN.mpo import MPO, SIS_MPO, occupancy_MPO, gapMPO
 from stochasticTN.stochasticDMRG import DMRG
-from stochasticTN.stochasticiDMRG import iDMRG
-from stochasticTN.stochasticDMRG_periodicBCs import periodicDMRG
 from stochasticTN.operations import *
 from stochasticTN.information_measures import *
